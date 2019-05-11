0d9bae1d188b8d94597958e8eb39b1bba9683357
Tosin427 <48648548+Tosin427@users.noreply.github.com>
2019-05-11 15:58:59 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+<!-- fonts library -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="js/caroufredsel.js" type="text/javascript"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Simon Diamond Entertainment</title>
+
+
+</head>
+
+
+<body>
+    
+    <header>
+        <div class="container">
+            <div class="row">
+                <a href="" class="logo"><img class="imagedimension" src="images/logo1.png" alt=""></a>
+                <nav>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About Us</a></li>
+                        <li><a href="">Services</a></li>
+                        <li><a href="">Clients</a></li>
+                        <li><a href="">Our Team</a></li>
+                        <li><a href="">Our Shop</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <section class="slider">
+        <ul class="slider-carousel" id="slider-carousel">
+            <li class="img1">
+                <h2>ENTERTAINMENT, <span>EVENTS,</span> MUSIC <span>ARTS</span></h2>
+                <p>We believe in creativity always</p>
+                <i class="fa fa-play-circle"></i>
+                <i class="fa fa-headphones-alt"></i>
+                <i class="fa fa-volume-up"></i>
+                <p><br>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br>Rem esse ullam dolor fugit quisquam facere expedita corporis reprehenderit ducimus <br>cupiditate voluptate iure impedit deserunt, voluptates eum quo ipsa! Amet, eum!
+                </p><br>
+                <a href="" class="btn btn-half">Register</a>
+                <a href="" class="btn btn-full">Shop Now</a>
+            </li>
+            <li class="img2">
+                <h2>ENTERTAINMENT, <span>EVENTS,</span> MUSIC <span>ARTS</span></h2>
+                <p>We believe in creativity always</p>
+                <i class="fa fa-play-circle"></i>
+                <i class="fa fa-headphones-alt"></i>
+                <i class="fa fa-volume-up"></i>
+                <p><br>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br>Rem esse ullam dolor fugit quisquam facere expedita corporis reprehenderit ducimus <br>cupiditate voluptate iure impedit deserunt, voluptates eum quo ipsa! Amet, eum!
+                </p><br>
+                <a href="" class="btn btn-half">Register</a>
+                <a href="" class="btn btn-full">Shop Now</a>
+            </li>
+            <li class="img3">
+                <h2>ENTERTAINMENT, <span>EVENTS,</span> MUSIC <span>ARTS</span></h2>
+                <p>We believe in creativity always</p>
+                <i class="fa fa-play-circle"></i>
+                <i class="fa fa-headphones-alt"></i>
+                <i class="fa fa-volume-up"></i>
+                <p><br>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br>Rem esse ullam dolor fugit quisquam facere expedita corporis reprehenderit ducimus <br>cupiditate voluptate iure impedit deserunt, voluptates eum quo ipsa! Amet, eum!
+                </p><br>
+                <a href="" class="btn btn-half">Register</a>
+                <a href="" class="btn btn-full">Shop Now</a>
+            </li>
+        </ul>
+    </section>
+
+    <section class="intro-area white" id="intro">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                    <h2>What we do</h2>
+                    <div class="sub-heading">
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.<br> Reiciendis illo obcaecati pariatur neque, aperiam ducimus rerum sed dignissimos vel,<br> sequi praesentium temporibus facilis architecto itaque veniam quia quaerat vitae fuga?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="intro-block">
+                        <span class="intro-icon"><i class="fa fa-paint-brush"></i></span>
+                        <h3>Create Sound Tracks</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam rerum reiciendis, maxime quia autem, eveniet nam, perspiciatis hic ipsa laborum sint nesciunt sapiente veniam obcaecati? Ratione debitis voluptates earum qui!</p>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="intro-block">
+                        <span class="intro-icon"><i class="fa fa-music"></i></span>
+                        <h3>Build Artist</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam rerum reiciendis, maxime quia autem, eveniet nam, perspiciatis hic ipsa laborum sint nesciunt sapiente veniam obcaecati? Ratione debitis voluptates earum qui!</p>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="intro-block">
+                        <span class="intro-icon"><i class="fa fa-drum"></i></span>
+                        <h3>Create Sound Tracks</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam rerum reiciendis, maxime quia autem, eveniet nam, perspiciatis hic ipsa laborum sint nesciunt sapiente veniam obcaecati? Ratione debitis voluptates earum qui!</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+    </section>
+
+    <script src="js/main.js"></script>
+</body>
+</html>
